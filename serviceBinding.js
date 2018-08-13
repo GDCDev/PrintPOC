81e270a4dcce305a25045706679367eab3c2fbb7
liu jun <jun2liu@abeam.com>
2018-08-13 07:25:20 +0000

    jun2liu

--- serviceBinding.js
+++ serviceBinding.js
@@ -1,5 +1,5 @@
 function initModel() {
-	var sUrl = "/here/goes/your/serviceurl/";
+	var sUrl = "/eanSet/";
 	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
 	sap.ui.getCore().setModel(oModel);
 }
